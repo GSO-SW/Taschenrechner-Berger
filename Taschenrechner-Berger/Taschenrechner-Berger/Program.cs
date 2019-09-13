0106ac24d6ddb82a910304aa862231f942750977
Vellensittich <michaelb17@schule.koeln>
2019-09-13 14:03:33 +0200

    Divisionsoperation hinzugefügt + Kommentare

--- Taschenrechner-Berger/Taschenrechner-Berger/Program.cs
+++ Taschenrechner-Berger/Taschenrechner-Berger/Program.cs
@@ -14,21 +14,30 @@ namespace Taschenrechner_Berger
 
             do
             {
+                //Willkommensgruß + Operationsabfrage
                 Console.WriteLine("Willkommen beim Taschenrechner!");
+                Console.WriteLine();
                 Console.WriteLine("Geben Sie die Rechenoperation (+,-,*,/) an");
                 Console.WriteLine("+ Addition");
                 Console.WriteLine("- Subtraktion");
                 Console.WriteLine("* Multiplikation");
+                Console.WriteLine("/ Divison");
+                Console.WriteLine();
+
                 char operation = Convert.ToChar(Console.ReadLine());
 
+                //Zahleneingabe
+                Console.WriteLine();
                 Console.WriteLine("Geben Sie die erste Zahl ein:");
                 double zahl1 = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine("Geben Sie die zweite Zahl ein:");
                 double zahl2 = Convert.ToDouble(Console.ReadLine());
 
+                //Ergebnis mit 0 initialisieren
                 double ergebnis = 0;
 
-                switch(operation)
+                //Ergebnis je nach Operation berechnen
+                switch (operation)
                 {
                     case '+':
                         ergebnis = zahl1 + zahl2;
@@ -39,10 +48,17 @@ namespace Taschenrechner_Berger
                     case '*':
                         ergebnis = zahl1 * zahl2;
                         break;
+                    case '/':
+                        ergebnis = zahl1 / zahl2;
+                        break;
                 }
 
+                //Ergebnis ausgeben
+                Console.WriteLine();
                 Console.WriteLine("Das Ergebnis ist: " + ergebnis);
+                Console.WriteLine();
 
+                //Abfrage ob erneut gerechnet werden will
                 Console.WriteLine("Wollen Sie erneut rechnen? Geben Sie J oder N ein.");
                 entscheidung = Console.ReadLine();
                 Console.Clear();
